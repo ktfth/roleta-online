c4ad9b88de4fa1c66e5894c39503e9355401d68a
Kaique da Silva <kaeyosthaeron@gmail.com>
2025-01-27 22:01:58 -0300

    Add ribbon do github

--- routes/index.tsx
+++ routes/index.tsx
@@ -68,6 +68,7 @@ export default function Home({ data: rooms }: PageProps<Room[]>) {
 
   return (
     <>
+      <a href="https://github.com/ktfth/roleta-online"><img decoding="async" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_green_007200.png" style={{position: 'absolute', top: 0, right: 0, border: 0}} alt="Faça um fork no GitHub" loading="lazy" /></a>
       <Head>
         <title>Roleta Online - Chat com Vídeo</title>
       </Head>
